457d0d7c97e4b32e7290febd975c2712116480ec
xiaoQuanTxue <673038225@qq.com>
2021-10-13 22:55:47 +0800

    修复方法导入错误

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ import '@/assets/scss/index.scss'
 import httpRequest from '@/utils/httpRequest' // api: https://github.com/axios/axios
 import { isAuth } from '@/utils'
 import cloneDeep from 'lodash/cloneDeep'
-import PubSub from pubsub - js
+import PubSub from "pubsub-js"
 
 Vue.use(VueCookie)
 Vue.config.productionTip = false
